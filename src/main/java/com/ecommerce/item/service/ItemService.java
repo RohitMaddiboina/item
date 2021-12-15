1259dd143470a8c1ebc1f4ad972c7577d014bd76
Rohityadav19 <rohitteja19@gmail.com>
2021-12-15 11:37:07 +0530

    improved pagination

--- src/main/java/com/ecommerce/item/service/ItemService.java
+++ src/main/java/com/ecommerce/item/service/ItemService.java
@@ -2,6 +2,8 @@ package com.ecommerce.item.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import com.ecommerce.item.model.Item;
 import com.ecommerce.item.model.ListOfItemsList;
 
@@ -10,22 +12,23 @@ public interface ItemService {
 	Item getItem(int itemID);
 
 	//This gets the list of list of items when user submits the category
-	ListOfItemsList getAllItems(String category);
+	Page<Item> getAllItems(String category,int index);
 
 	//This gets the list of list of items when user wants particular item type from category
-	ListOfItemsList getCategoryItemsWithItemType(String category, List<String> itemTypes);
+	Page<Item> getCategoryItemsWithItemType(String category, List<String> itemTypes,int index);
 
 	//This gets a list of list of items of a category based on price range set by user
-	ListOfItemsList getCategoryItemsWithPrice(String category, float low, float high);
+	Page<Item> getCategoryItemsWithPrice(String category, float low, float high,int index);
 
 	//This gets the List of List of items from a category of a itemType within the price range
-	ListOfItemsList getCategoryItemsWithItemTypePrice(String category, List<String> itemTypes, float low, float high);
+	Page<Item> getCategoryItemsWithItemTypePrice(String category, List<String> itemTypes, float low, float high,
+			int index);
 
 	//Gets all the itemsTypes present in the category
 	List<String> getDistinctItemType(String category);
 
 	//Gets List Of items list based on the keyword given by users
-	ListOfItemsList getItemsKeyWord(String keyword);
+	Page<Item> getItemsKeyWord(String keyword,int index);
 
 	Item addQuantityToItems(int itemId, int quantity);
 
